775c0bfcfaae027c7cb1c658233e2c8ea03b7f3e
gzgz2018 <gzgzgz2018@gmail.com>
2017-07-08 12:54:20 -0300

    zz
    Signed-off-by: gzgz2018 <gzgzgz2018@gmail.com>

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,19 @@
 function Mostrar()
 {
+    //  Al ingresar una edad solo debemos informar si la persona NO es adolescente.
+    //
 //tomo la edad  
-
-
+var edad;
+edad=parseInt(document.getElementById("edad").value);
+  if (edad<13 || edad>17)
+{
+alert("No es adolescente");
+}
 }//FIN DE LA FUNCIÓN
+// también se puede reutilizarla lógica del ejercicio nº 4 y negarlo
+//Ejemplo : if (!(edad>=13 && edad<=17))
+            //{
+//             alert("No es adolescente");
+            //}
+//
+
